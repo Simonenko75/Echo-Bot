140c4d331d47d8509555815a11bab00485a23293
Simonenko75 <simonenkonikita00@gmail.com>
2023-02-13 16:02:40 +0200

    "second commit"

--- auth_data.py
+++ auth_data.py
@@ -1,2 +1,2 @@
-tg_bot_token = "5400393089:AAHL2w3uRKSYN5jQvQpCuTSJiVALkfDbsYI"
+tg_bot_token = "5400393089:AAF79dhvNUI0EsogOa1T_toBAfHsbt_ugoI"
 App_URL = f"https://git.heroku.com/my-bot-1.git/{tg_bot_token}"
